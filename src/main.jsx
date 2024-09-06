c118ac59d34e67f4364f278f4ba5ce19844fc41f
Gusta346 <gustavoribeiro4523@gmail.com>
2024-09-06 15:48:51 -0300

    Criando rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'    
 import App from './App.jsx'
-import './index.css'
+import Home from './routes/Home.jsx'
+import Error from './routes/Error.jsx'
+
+const router = createBrowserRouter([
+  {
+    //Elemento Pai
+    path:'/', element:<App/>,
+    errorElement:<Error/>,
+
+    //Elementos filhos
+    children:[
+      {path:'/', element:<Home/>},
+      
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
