74de45b4990c718cb819737e954a3d7a3679f389
Gusta346 <gustavoribeiro4523@gmail.com>
2024-09-06 16:05:32 -0300

    Criando Styled-components,Criando Footer e aplicando Css via styled

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Rodape } from "../../styled"
+
+const Footer = () =>{
+    return(
+        <Rodape>
+        <p>2024@-Todos os direitos reservados</p>
+        </Rodape>
+    )
+}
+
+export default Footer
---
+++ styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Rodape = styled.footer `
+
+    /* APENAS TESTE PARA FUNCIONAMENTO, MUDAR ASSIM QUE POSSIVEL */
+    width: 100%;
+    min-height: 5vh;
+    padding: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow:1;
+    
+    p{
+        
+        color: blue;
+    }
+
+
+
+
+`
