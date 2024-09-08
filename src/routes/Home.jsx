cb213e4d8f50ed147982b3f923d0a64c7a5e7af0
Gabriel <gvqsilva@gmail.com>
2024-09-07 21:54:57 -0300

    Adicionando uma breve descrição sobre a loja

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,21 +1,19 @@
 import { MainGeral } from "../styles/styled"
 
-
 const Home = () =>{
     return(
         <MainGeral>
             <div className="home-container">
-      <div className="video-container">
-        <video className="home-video" autoPlay muted loop>
-          <source src="./src/video/video.mp4" type="video/mp4" />
-          Seu navegador não suporta o elemento de vídeo.
-        </video>
-      </div>
+                <div className="video-container">
+                    <video className="home-video" autoPlay muted loop>
+                        <source src="./src/video/video.mp4" type="video/mp4" />
+                    </video>
+                </div>
 
-      <h1>Bem-vindo à EsporteElite</h1>
-      <p>Estamos entusiasmados em receber você em nossa loja online, onde o esporte é mais do que uma paixão, é um estilo de vida!</p>
-      <h2>Alguns dos nossos produtos</h2>
-    </div>
+                <h1>Bem-vindo à EsporteElite</h1>
+                <p>Estamos entusiasmados em receber você em nossa loja online, onde o esporte é mais do que uma paixão, é um estilo de vida!</p><br/>
+                <p>Estamos felizes em ter você aqui! Na nossa loja online, oferecemos tudo o que você precisa para seus treinos e competições, com produtos de alta qualidade das melhores marcas. Nosso objetivo é tornar sua experiência de compra fácil e agradável. Agradecemos sua visita e esperamos que encontre exatamente o que precisa para alcançar seus objetivos esportivos.</p>
+            </div>
         </MainGeral>
     )
 }
